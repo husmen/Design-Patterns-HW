2d25c67be0579ecd58834b59570fa8329a49e124
husmen <husmen93@gmail.com>
2019-05-31 18:10:27 +0300

    added object pool pattern

---
+++ src/object_pool/object_pool.py
@@ -0,0 +1,59 @@
+import logging
+
+class CamerasPool:
+    """
+    Manage Reusable objects for use by Client objects.
+    """
+
+    def __init__(self, size):
+        self._reusables = [Camera(_) for _ in range(size)]
+
+    def acquire(self):
+        try:
+            return self._reusables.pop()
+        except:
+            logging.error("All Cameras Are Busy!")
+            return -1
+
+    def release(self, reusable):
+        self._reusables.append(reusable)
+
+
+class Camera:
+    """
+    Unique Camera with unique ID
+    """
+
+    def __init__(self, id):
+        self._id = id
+        self._params = None
+        self._frame = None
+        
+    def get_id(self):
+        return self._id
+    
+    def start(self):
+        pass
+    
+    def stop(self):
+        pass
+
+    def get_frame(self):
+        return self._frame
+
+    def new_frame(self, img):
+        self._frame = img
+
+
+if __name__ == "__main__":
+    cameras = CamerasPool(2)
+    cam1 = cameras.acquire()
+    cam2 = cameras.acquire()
+    print(cam1.get_id())
+    print(cam2.get_id())
+    cameras.release(cam1)
+    del cam1
+    cam3 = cameras.acquire()
+    print(cam2.get_id())
+    print(cam3.get_id())
+    cam4 = cameras.acquire()
